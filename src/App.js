68333fb410eaf7dbb093c3356be30055b16c475f
Leo Yousif <leoyousif@gmail.com>
2020-04-27 02:39:27 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
+//import { withAuthenticator } from 'aws-amplify-react'
+//import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 //Amplify.configure(aws_exports);
 
